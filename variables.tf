588f5e110866738a65ff0487f419b62f32a300f6
PK Iyer <92050296+piyerf5@users.noreply.github.com>
2023-03-28 17:15:38 -0400

    added variables for k8s cluster

--- variables.tf
+++ variables.tf
@@ -19,3 +19,9 @@ variable "ssh_public_key" {
 variable instanceSuffix {
     type = string
 }
+variable k8s_cluster_name { 
+    type = string
+}
+variable k8s_cluster_namespace {
+    type = string
+}
